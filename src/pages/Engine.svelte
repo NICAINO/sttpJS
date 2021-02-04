a3a9b64d343bc877ea8493e56f40430e70393bb5
LSaanen <liamsaanen@gmail.com>
2021-02-04 22:30:44 +0100

    Btje uitleg toegevoegd. Ga maar lezen

--- src/pages/Engine.svelte
+++ src/pages/Engine.svelte
@@ -40,40 +40,45 @@
             grid_value = value;
         });
 
+    //Als er een key wordt gepressed checkt ie welke key het is
     function handleKeydown(event) {
 		let key = event.key;
         let keyCode = event.keyCode;
         
-        if (keyCode === 32) {
+        if (keyCode === 32) {               //Als je op spatie drukt plaats je een road
             placePiece(selected, Road)
         }
     }
     
+    //Met deze functie wordt nog nix gedaan. Mr ik dacht iets te maken zodat er wordt gekeken wat mogelijke cellen zijn om je shit nrtoe te plaatsen
     const checkPossibleCells = (x,y) => {
         let array = grid_value[selectedPiece.y][selectedPiece.x]
         let height = array.length - selectedPiece.z
-        if (height > 5) {
-            deselectPiece(selectedPiece.x, selectedPiece.y)
-        } else {
-            for (let i = 1; i < (height+1); i++) {
-                let possibleLeft = x - i
-                let possibleRight = x + i
-                let possibleDown = y + i
-                let possibleUp = y - i
-                possibleCells.push({possibleLeft, y}, {possibleUp, x}, {possibleRight, y}, {possibleDown, x})
-            }
+        //Deze shit is kk lelijk dus je moet ff helpen met mooi maken
+        for (let i = 1; i < (height+1); i++) {
+            let possibleLeft = x - i
+            let possibleRight = x + i
+            let possibleDown = y + i
+            let possibleUp = y - i
+            possibleCells.push({possibleLeft, y}, {possibleUp, x}, {possibleRight, y}, {possibleDown, x})
         }
     }
 
+    //Dit is zeg maar dat er een nieuwe piece wordt geplaatst
     const placePiece = (selected, type) => {
+        //Eerst checken of de speler nog pieces heeft
         if (currentPlayer.pieces > 0) {
             let topDestinyCell = grid_value[selected.y][selected.x].slice(-1)
+            // Dan of de bovenste een wall is. Mss dit in een aparte functie plaats laten vinden
             if (topDestinyCell[0] && topDestinyCell[0].type === 'wall') {
                 console.log("Wall")
             } else {
+                //Geen wall dan voert ie deze shit uit
                 let succes = pieceAction(selected.x, selected.y, type, currentPlayer.color)
                 if (!succes) {
+                    //Als ie een piece heeft kunnen plaatsen dan heeft de speler een peice minder 
                     deductPiece()
+                    //En is zn beurt voorbij
                     endTurn()
                 } else {
                     console.log('Still your turn')
@@ -84,24 +89,31 @@
         }      
     }
 
+    //Dit is het moven van pieces/stacks
     const movePiece = (x,y) => {
         let array = grid_value[selectedPiece.y][selectedPiece.x]
         let topDestinyCell = grid_value[y][x].slice(-1)
+        //Eerst checken of de stack die je wilt verplaatsen niet de stacklimit overschreid
         if ((array.length - selectedPiece.z) > 5) {
             console.log('Stack to high')
+        //Dan weer checken of er een wall staat op de plak waar je het naartoe wilt verplaatsen
         } else if (topDestinyCell && topDestinyCell[0].type === 'wall') {
             console.log("Wall")
+        //De stack splicen uit huidige locatie en een voor een reassignen op nieuwe locatie
+        //Deze shit is nog fk verneukt, mr dat leg ik je nog wel uit
         } else {
             let items = array.splice(selectedPiece.z)
             for (let i = 0; i < items.length; i++) {
                 pieceAction(x, y, items[i], items[i].color)
             }
-            
         }
+        //Als alles is gedaan, dan deselect ie de geslecteerde piece
         deselectPiece(selectedPiece.x, selectedPiece.y)
     }
 
+    //Deze functie 'plaatst' de piece echt, mr gebruik ik ook om stacks te reassignen als het ware
     const pieceAction = (x, y, type, color) => {
+        //Wordt gecheckt of de toren die er staat niet al te hoog is
         if (grid_value[y][x].length < maxHeigth) {
             const piece = {
                 type: type.type,
@@ -119,12 +131,16 @@
         }
     }
 
+    //Gwn de shit waarbij je een cell select en die daarmee selected cell maakt
     const selectSquare = (x,y) => {
         selected.x = x
         selected.y = y
 
+        //Checken of er een piece is geselect 
         if (selectedPiece.x !== null || selectedPiece.y !== null || selectedPiece.z !== null) {
+            //Checken of die geselecte piece een andere locatie heeft dan de geselecteerde cell
             if (selectedPiece.x !== x || selectedPiece.y !== y) {
+                //Zo ja, dan moved ie de stack emt movePiece
                 movePiece(x,y)
             } else {
                 console.log('Same cell')
@@ -134,11 +150,17 @@
         }
     }
 
+    //Select ie een piece
     const selectPiece = (location,x,y,top) => {
+        //Gecheckt of de active player wel de stack owned waarin hij een piece probeert te selecteren
         if (top === currentPlayer.color) {
+            //Checken of de peice niet al is de geselecteerd
             if (selectedPiece !== location) {
+                //Piece wordt gemarkeerd als selectedPiece
                 selectedPiece = location
+                //En dus deze functie die nog niet echt iets doet
                 checkPossibleCells(location.x, location.y)
+            //Als de piece al was geselecteerd dan nu niet meer
             } else {
                 deselectPiece(x,y)
             }
@@ -147,23 +169,29 @@
         }
     }
 
+    //Het deselecteren van een piece
     const deselectPiece = (x,y) => {
+        //Als deselect func wordt aangeroepen en je bent toevllig in hetzelfde vakje, idk wnnr mr vast ooit. Dan gebeurt er nix
         if (selectedPiece.x !== x || selectedPiece.y !== y) {
             selectSquare(x,y)
+        //Anders deselect ie 
         } else {
             selectedPiece = {
                 x: null,
                 y: null,
                 z: null,
             }
+            //Heeft weer met die nix doen functie te maken
             possibleCells = []
         }
     }
 
+    //Is gwn dat er een piece wordt afgetrokken van het aantal pieces dat een speler heeft
     const deductPiece = () => {
         currentPlayer.pieces = currentPlayer.pieces-1
     }
 
+    //Wisselt van currentplayer
     const endTurn = () => {
         if (currentPlayer == players.player1) 
             {currentPlayer = players.player2}
